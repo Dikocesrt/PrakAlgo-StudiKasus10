eda4c5f45db3b3d78a8c85ed2c08228d2d987945
Diko Cesartista <dikocesrt@gmail.com>
2022-06-22 15:46:28 +0700

    memisahkan input proses output dan menambahkan skor

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
-#include "Model/Finalis.h"
+#include "Model/Input.h"
+#include "Model/Proses.h"
+#include "Model/Output.h"
 using namespace std;
 
 int main(){
-    Finalis objectFinalis = Finalis();
+    Input objectInput = Input();
+    Proses objectProses = Proses();
+    Output objectOutput = Output();
     string nama[100];
     int long nim[100];
-    objectFinalis.input(nama, nim);
-    objectFinalis.proses(nama, nim);
-    objectFinalis.output(nama,nim);
+    int skor[100];
+    objectInput.input(nama, nim, skor);
+    objectProses.proses(nama, nim, skor);
+    objectOutput.output(nama,nim, skor);
     return 0;
 }
